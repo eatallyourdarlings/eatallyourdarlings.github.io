66758321bea11eaa0c6160d3fae10dfa4db3de23
eatallyourdarlings <eatallyourdarlings@gmail.com>
2024-12-17 12:58:19 +1100

    finish cs bookmarklet, trim dither

--- etc/bookmarklets.js
+++ etc/bookmarklets.js
@@ -1,19 +1,36 @@
+// template
 javascript: (() => {  })();
 
+// example
 javascript: (() => {
     return '<h1 style="color: white; background-color: black;">Hello, World!</h1>';
 })();
 
-// select a canvas element and print to console
-javascript: (() => { const canvas = document.querySelector('canvas'); console.log(canvas); })();
 
+// screenshot canvas
 
+javascript: (() => { 
+let a = document.createElement('a'); 
+a.href = document.querySelector('canvas').toDataURL('image/png');
+let date = new Date(); 
+let datetime = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+"-"+date.getHours()+"-"+date.getMinutes()+"-"+date.getSeconds();
+a.download = document.title+"-"+datetime+".png"; 
+a.setAttribute('rel', 'noopener noreferrer');
+// a.setAttribute('target', '_blank');
+document.body.appendChild(a); 
+a.innerHTML = "download";
+a.click(); 
+document.body.removeChild(a);
+})();
+
+/////// past attempts
 
 //screenshot iframe
 const iframe = document.querySelector('iframe'); console.log(iframe); console.log(iframe.contentDocument.querySelector('canvas'));
 
 javascript: (() => { const iframe = document.querySelector('iframe'); console.log(iframe); console.log(iframe.contentDocument.querySelector('canvas')); })();
 
+javascript: (() => { 
 const canvas = document.querySelector('canvas'); console.log(canvas); dataURL = canvas.toDataURL('image/png'); 
 const a = document.createElement('a'); 
 a.href = dataURL; 
@@ -24,6 +41,7 @@ a.setAttribute('target', '_blank');
 document.body.appendChild(a); 
 a.click(); 
 document.body.removeChild(a); 
+})
 
 javascript: (() => { 
     var canvas = document.querySelector('canvas'); 
@@ -44,16 +62,3 @@ javascript: (() => {
     img.src = dataURL;
     document.body.appendChild(img);
 })();
-
-function screenshot_canvas (){
-var canvas = document.querySelector('canvas'); 
-var dataURL = canvas.toDataURL('image/png'); 
-var a = document.createElement('a'); 
-a.href = dataURL; 
-var date = new Date(); 
-var datetime = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+"-"+date.getHours()+"-"+date.getMinutes()+"-"+date.getSeconds();
-a.download = document.title+datetime+".png"; 
-a.setAttribute('rel', 'noopener noreferrer');
-a.setAttribute('target', '_blank');
-document.body.appendChild(a); 
-a.click(); }
